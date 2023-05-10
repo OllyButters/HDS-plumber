44921437dbc7a03220b22a2092b50e32309db723
OllyButters <olly.butters@newcastle.ac.uk>
2023-05-10 11:06:04 +0100

    added more explanatory text

--- exercise_1_api_example.R
+++ exercise_1_api_example.R
@@ -1,31 +1,42 @@
 ##########################################
 # exercise 1 API example
 # How many people are in space right now?
-# Run this line at a time.
+# Run this file one line at a time (Ctrl - Enter).
 ##########################################
 
-library(httr)
-library(jsonlite)
+library(httr)             # Needed for request/response with API
+library(jsonlite)         # Needed to decode the response data from the API
 
 
-# Query the API
+# Send request to the API and get a response back
 response = GET("http://api.open-notify.org/astros.json")
 
-# Look at the response.
+# Print the response.
 response
 
-# This is actually just some header info. Look at the Environment tab to see what
-# else is in the response object.
+# This is actually just some header info. Note the "Status: 200" line, which 
+# tells us the request/response  executed successfully. 
+# We can also see the "Content-Type: application/json" line which tells us the
+# format of the returned data.
 
-# We want the content
+# Look at the Environment tab to see what else is in the response object (click 
+# on the blue arrow next to "response"). There is a lot of metadata in here 
+# telling us how the request/response went, what time it happened etc. We are
+# only really interested in the content section.
+
+# Print the the content section of the response
 response$content
 
 # But the content is in binary, so convert the response to R data object
 data = fromJSON(rawToChar(response$content))
 
-# Look at the data. Note it is a list.
+# Print the data. Note that it is a list with 3 items (message, number, people)
 data
 
-# Just get the list of people.
+# Print out just the people.
 data$people
+
+# Now you know who is in space right now!
+
+
 
